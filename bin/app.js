c024878ec7718537b1316073051a591f5a31cccd
rhass99 <rhass99@gmail.com>
2017-12-31 00:23:46 +0400

    both nlp working

--- bin/app.js
+++ bin/app.js
@@ -8,7 +8,7 @@ const server = http.createServer(service);
 server.listen(3000);
 
 slackClient(witClient, x => x.start());
-//slackClient(apiaiClient, x => x.start());
+// slackClient(apiaiClient, x => x.start());
 
 server.on('listening', () => {
   console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')}`);
